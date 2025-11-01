6086070e5525e9ab81819e005d59436bd3660081
satoshi25 <satoshi25.kr@gmail.com>
2025-11-01 19:50:32 +0900

    Streamlit data flow 1

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
 import streamlit as st
-import time
 
-st.header("Hello world!")
+is_admin = False
 
-st.button("Click me please!")
+st.header("Hello!")
 
-st.text_input("Write your API KEY", max_chars=20)
+name = st.text_input("What is your name?")
 
-st.feedback("faces")
+if name:
+    st.write(f"Hello {name}")
+    is_admin = True
 
-with st.sidebar:
-    st.badge("Badge 1")
-
-tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
-
-with tab1:
-    st.header("Agent 1")
-with tab2:
-    st.header("Agent 2")
-with tab3:
-    st.header("Agent 3")
-
-st.divider()
-
-with st.chat_message("ai"):
-    st.text("Hello!")
-    with st.status("Agent is using tool") as status:
-        time.sleep(1)
-        status.update(label="Agent is searching the web...")
-        time.sleep(2)
-        status.update(label="Agent is reading the page...")
-        time.sleep(3)
-        status.update(state="complete")
-with st.chat_message("human"):
-    st.text("hi")
-
-st.chat_input("Write a message for the assistant.", accept_file=True)
+print(is_admin)
