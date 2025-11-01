5ca2753fbe7cb6d2830434e541f67c68dc0111cc
satoshi25 <satoshi25.kr@gmail.com>
2025-11-01 19:19:08 +0900

    use streamlit

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from chatgpt-clone!")
+import streamlit as st
+import time
 
+st.header("Hello world!")
 
-if __name__ == "__main__":
-    main()
+st.button("Click me please!")
+
+st.text_input("Write your API KEY", max_chars=20)
+
+st.feedback("faces")
+
+with st.sidebar:
+    st.badge("Badge 1")
+
+tab1, tab2, tab3 = st.tabs(["Agent", "Chat", "Output"])
+
+with tab1:
+    st.header("Agent 1")
+with tab2:
+    st.header("Agent 2")
+with tab3:
+    st.header("Agent 3")
+
+st.divider()
+
+with st.chat_message("ai"):
+    st.text("Hello!")
+    with st.status("Agent is using tool") as status:
+        time.sleep(1)
+        status.update(label="Agent is searching the web...")
+        time.sleep(2)
+        status.update(label="Agent is reading the page...")
+        time.sleep(3)
+        status.update(state="complete")
+with st.chat_message("human"):
+    st.text("hi")
+
+st.chat_input("Write a message for the assistant.", accept_file=True)
