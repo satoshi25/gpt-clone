873e3393c70e356a1141b5a9c96bd027302c25b2
satoshi25 <satoshi25.kr@gmail.com>
2025-11-02 17:24:41 +0900

    Edit sidebar memory

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if "session" not in st.session_state:
 session = st.session_state["session"]
 
 async def run_agent(message):
-    stream = Runner.run_streamed(agent, message)
+    stream = Runner.run_streamed(agent, message, session=session)
 
     async for event in stream.stream_events():
         if event.type == "raw_response_event":
@@ -31,15 +31,15 @@ async def run_agent(message):
                 with st.chat_message("ai"):
                     st.write(event.data.delta)
 
-with st.sidebar:
-    reset = st.button("Reset memory")
-    if reset:
-        asyncio.run(session.clear_session())
-    st.write(asyncio.run(session.get_items()))
-
 prompt = st.chat_input("Write a message for your assistant.")
 
 if prompt:
     with st.chat_message("human"):
         st.write(prompt)
     asyncio.run(run_agent(prompt))
+
+with st.sidebar:
+    reset = st.button("Reset memory")
+    if reset:
+        asyncio.run(session.clear_session())
+    st.write(asyncio.run(session.get_items()))
