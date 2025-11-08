223134fa6a39e1a769a349e6a71216ebbfd932bb
satoshi25 <satoshi25.kr@gmail.com>
2025-11-08 16:31:54 +0900

    Add WebSearchTool

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import dotenv
 dotenv.load_dotenv()
 import asyncio
 import streamlit as st
-from agents import Agent, Runner, SQLiteSession
+from agents import Agent, Runner, SQLiteSession, WebSearchTool
 
 if "agent" not in st.session_state:
     st.session_state["agent"] = Agent(
         name="ChatGPT Clone",
         instructions="""
         You are a helpful assistant.
-        """
+
+        You have access to the following tools:
+            - WebSearchTool: Use this when the user asks a questions that isn't in your training data. use this to learn about current events.
+        """,
+        tools=[WebSearchTool()]
     )
 
 agent = st.session_state["agent"]
@@ -26,12 +30,16 @@ async def paint_history():
     messages = await session.get_items()
 
     for message in messages:
-        with st.chat_message(message["role"]):
-            if message["role"] == "user":
-                st.write(message["content"])
-            else:
-                if message["type"] == "message":
-                    st.write(message["content"][0]["text"])
+        if "role" in message:
+            with st.chat_message(message["role"]):
+                if message["role"] == "user":
+                    st.write(message["content"])
+                else:
+                    if message["type"] == "message":
+                        st.write(message["content"][0]["text"])
+        if "type" in message and message["type"] == "web_search_call":
+            with st.chat_message("ai"):
+                st.write("🔍 Search the web ...")
 
 asyncio.run(paint_history())
 
